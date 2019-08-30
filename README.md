3d8ef652f286aa10095c68ee5b00c4209ea98224
Jeremey van Uden <tasje34@gmail.com>
2019-08-30 13:40:47 +0200

    Sweep project clean

--- README.md
+++
@@ -1,7 +0,0 @@
-#Canadian Addition
-
-## Installing
-To Install this project run npm install.
-
-## Running
-To run the project type 'nodemon' in the terminal, the server should start on 127.0.0.1:3000.
--- router.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app){
-    // To add a route use this: app.use('ROUTENAME', require('PATH TO ROUTE FILE'));
-    // ./ is used to set the starting point from the file itself.
-    app.use('/', require('./routes/index'));
-    app.use('/game', require('./routes/game'));
-}
