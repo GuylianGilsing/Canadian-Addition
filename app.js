91fd20df25de1c08b4b9b25bbe21031fc19f6675
Jeremey van Uden <tasje34@gmail.com>
2019-09-06 09:31:35 +0200

    removed test emits from app.js

--- app.js
+++ app.js
@@ -6,21 +6,6 @@ socket.on("roomRequest", ()=>{
     socket.emit("joinRoom", url)
 });
 
-socket.on("roomCreated", (msg)=>{
-    console.log(msg);
-});
-socket.on("joinedUser", (msg)=>{
-    console.log(msg);
-});
-
 socket.on("disconnect", (msg)=>{
     window.location =("/");
-});
-
-socket.on("leftUser", (msg)=>{
-    console.log(msg);
-});
-
-socket.on("disconnect", (msg)=>{
-    console.log(msg);
 });
